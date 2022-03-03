d44d27d6ab7cc17cb81119a879dd999ba95b182d
Jordan Yee <jordantyee@berkeley.edu>
2022-03-02 20:02:25 -0800

    Initial Commit

---
+++ newsfeed/newsfeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsfeedApp.swift
+//  newsfeed
+//
+//  Created by Jordan Yee on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct newsfeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
