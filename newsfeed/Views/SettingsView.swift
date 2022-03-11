79bd22a6f1b351aad56e2edaf03d2671fbd041d2
Jordan Yee <jordantyee@berkeley.edu>
2022-03-10 20:42:38 -0800

    commit 1

---
+++ newsfeed/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  newsfeed
+//
+//  Created by Jordan Yee on 3/10/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Screen")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
